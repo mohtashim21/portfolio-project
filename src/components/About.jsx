e0a9ce68ae6495e911d2b50941e6080844d5fb34
mohtashim21 <mohteshim3276@gmail.com>
2024-04-18 15:02:06 +0530

    added About page and Techstack component

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Techstack from './TechStack'
+import Particle from './Particle'
+
+
+const About = () => {
+  return(
+   <div>
+    <Particle />
+    <Techstack />
+   </div>
+    
+  )
+}
+
+export default About
