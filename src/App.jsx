bfc5dfcd89826f44ed87dfc04f7cb5641c8391b3
mohtashim21 <mohteshim3276@gmail.com>
2024-04-19 23:19:26 +0530

    added projects page and added new features using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import HomePage from './pages/HomePage'
-import AboutPage from './pages/AboutPage'
-import ProjectsPage from './pages/ProjectsPage'
-import ResumePage from './pages/ResumePage'
+import Navbar from './components/Navbar';
+import HomePage from "./pages/HomePage";
+import AboutPage from './pages/AboutPage';
+import ProjectsPage from './pages/ProjectsPage';
+import ResumePage from './pages/ResumePage';
+import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      {/* <HomePage /> */}
-      {/* <AboutPage /> */}
-      <ProjectsPage />
-      {/* <ResumePage /> */}
-    </div>
+    <>
+
+   <BrowserRouter> 
+       <Navbar />
+      <Routes>
+       <Route path='/' element={<HomePage />}/>
+       <Route path='/about' element={<AboutPage />}/>
+       <Route path='/projects' element={<ProjectsPage />}/>
+       <Route path='/resume' element={<ResumePage />}/>
+     </Routes>
+   </BrowserRouter>
+  </>
   )
 }
 
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Project from '../components/Project'
+import mental_health_project from "../assets/projects/mental_health.png";
+import shopify_project from "../assets/projects/shopify.png";
+import employee_management_project from "../assets/projects/employee_management.png";
 
 const ProjectsPage = () => {
   return (
     <div className='flex gap-10 mt-10 justify-center'>
-        <Project />
-        <Project />
-        <Project />
+        <Project title="Mental Health Consulting Project" desc="It is a Consulting Web App for teenagers struggling with mental health issues." image={mental_health_project} link="https://mental-health-therapy.netlify.app/" />
+
+        <Project title="Shopify" desc="an ecommerce website for clothes of men and women." image={shopify_project} link="https://github.com/mohtashim21/shopify" />
+
+        <Project title="Employee Managenent System" desc="an Employee Management system which can store read write info of emoloyees." image={employee_management_project} link="https://employee-management-react-redux.netlify.app/" />
     </div>
   )
 }
