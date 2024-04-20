6c75d00afde8bf22e7145a18a8359ae5b98098cb
mohtashim21 <mohteshim3276@gmail.com>
2024-04-20 13:44:00 +0530

    made responsive

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 const Project = ({ title, image, desc, link }) => {
   return (
     <div>
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="max-w-sm mb-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <div>
           <img className="rounded-t-lg" src={image} alt="project image" />
         </div>
@@ -19,7 +19,7 @@ const Project = ({ title, image, desc, link }) => {
           </p>
           <NavLink
             to={link}
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             View
             <svg
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -3,16 +3,20 @@ import Project from '../components/Project'
 import mental_health_project from "../assets/projects/mental_health.png";
 import shopify_project from "../assets/projects/shopify.png";
 import employee_management_project from "../assets/projects/employee_management.png";
+import Particle from '../components/Particle';
 
 const ProjectsPage = () => {
   return (
-    <div className='flex gap-10 mt-10 justify-center'>
+    <>
+    <Particle />
+    <div className='mx-5 sm:flex sm:gap-10 sm:mt-10 sm:justify-center'>
         <Project title="Mental Health Consulting Project" desc="It is a Consulting Web App for teenagers struggling with mental health issues." image={mental_health_project} link="https://mental-health-therapy.netlify.app/" />
 
         <Project title="Shopify" desc="an ecommerce website for clothes of men and women." image={shopify_project} link="https://github.com/mohtashim21/shopify" />
 
         <Project title="Employee Managenent System" desc="an Employee Management system which can store read write info of emoloyees." image={employee_management_project} link="https://employee-management-react-redux.netlify.app/" />
     </div>
+    </>
   )
 }
 
