07d00f014d15c6b39a070bd83c5c35313f496f8b
mohtashim21 <mohteshim3276@gmail.com>
2024-05-27 14:52:52 +0530

    change button styling

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -19,7 +19,7 @@ const Project = ({ title, image, desc, link }) => {
           </p>
           <NavLink
             to={link}
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 hover:bg-purple-600 rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 "
           >
             View
             <svg
